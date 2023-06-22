1709cc0ebf072485fede8d4fe7e3237922b76e04
crazyads69 <leminhtri11a1@gmail.com>
2023-06-22 22:26:40 +0700

    Lmao

---
+++ Queue.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int info;
+    struct Node *pNext;
+};
+typedef struct Node NODE;
+
+struct Queue
+{
+    NODE *pHead;
+    NODE *pTail;
+};
+typedef struct Queue QUEUE;
+
+void CreateQueue(QUEUE &q)
+{
+    q.pHead = q.pTail = NULL;
+}
+
+int IsEmptyQueue(QUEUE q)
+{
+    if (q.pHead == NULL)
+        return 1;
+    return 0;
+}
+
+NODE *CreateNode(int x)
+{
+    NODE *p = new NODE;
+    if (p == NULL)
+        return NULL;
+    p->info = x;
+    p->pNext = NULL;
+    return p;
+}
+
+void EnQueue(QUEUE &q, NODE *p)
+{
+    if (IsEmptyQueue(q) == true)
+        q.pHead = q.pTail = p;
+    else
+    {
+        q.pTail->pNext = p;
+        q.pTail = p;
+    }
+}
+
+int DeQueue(QUEUE &q)
+{
+    if (IsEmptyQueue(q) == true)
+        return 0;
+    NODE *p = q.pHead;
+    int x = p->info;
+    q.pHead = q.pHead->pNext;
+    delete p;
+    return x;
+}
+
+int Front(QUEUE q)
+{
+    if (IsEmptyQueue(q) == true)
+        return 0;
+    return q.pHead->info;
+}
+
+void PrintStack(QUEUE q)
+{
+    while (IsEmptyQueue(q) == false)
+    {
+        cout << DeQueue(q) << " ";
+    }
+}
+
+int main()
+{
+    QUEUE q;
+    CreateQueue(q);
+    NODE *p = CreateNode(1);
+    EnQueue(q, p);
+    p = CreateNode(2);
+    EnQueue(q, p);
+    p = CreateNode(3);
+    EnQueue(q, p);
+    p = CreateNode(4);
+    EnQueue(q, p);
+    p = CreateNode(5);
+    EnQueue(q, p);
+    PrintStack(q);
+    return 0;
+}
--- Stack.cpp
+++ Stack.cpp
@@ -1,2 +1,93 @@
 #include <iostream>
 using namespace std;
+
+struct Node
+{
+    int info;
+    struct Node *pNext;
+};
+typedef struct Node NODE;
+
+struct Stack
+{
+    NODE *pHead;
+};
+typedef struct Stack STACK;
+
+void CreateStack(STACK &s)
+{
+    s.pHead = NULL;
+}
+
+NODE *CreateNode(int x)
+{
+    NODE *p = new NODE;
+    if (p == NULL)
+        return NULL;
+    p->info = x;
+    p->pNext = NULL;
+    return p;
+}
+
+int IsEmpty(STACK s)
+{
+    if (s.pHead == NULL)
+        return 1;
+    return 0;
+}
+
+void Push(STACK &s, NODE *p)
+{
+    if (IsEmpty(s) == true)
+        s.pHead = p;
+    else
+    {
+        p->pNext = s.pHead;
+        s.pHead = p;
+    }
+}
+
+int Pop(STACK &s)
+{
+    if (IsEmpty(s) == true)
+        return 0;
+    NODE *p = s.pHead;
+    int x = p->info;
+    s.pHead = s.pHead->pNext;
+    delete p;
+    return x;
+}
+
+int Top(STACK s)
+{
+    if (IsEmpty(s) == true)
+        return 0;
+    return s.pHead->info;
+}
+
+void Output(STACK s)
+{
+    while (IsEmpty(s) == false)
+    {
+        int x;
+        cout << Pop(s) << " ";
+    }
+}
+
+int main()
+{
+    STACK s;
+    CreateStack(s);
+    NODE *p = CreateNode(1);
+    Push(s, p);
+    p = CreateNode(2);
+    Push(s, p);
+    p = CreateNode(3);
+    Push(s, p);
+    p = CreateNode(4);
+    Push(s, p);
+    p = CreateNode(5);
+    Push(s, p);
+    Output(s);
+    return 0;
+}
